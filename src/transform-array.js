50d3bec7fecc524bc16bffccaac6af55e7419838
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-01-23 02:55:43 +0300

    refactor: remove unnecessary nesting

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,38 +2,33 @@ const CustomError = require('../extensions/custom-error');
 
 module.exports = function transform(arr) {
   if (!Array.isArray(arr)) throw new Error('Wrong type');
-  if (arr.length === 0) return [];
+
   let transformedArray = [];
   let isCommand = false;
 
-  for (let i = 0; i < arr.length; i++)
+  for (let i = 0; i < arr.length; i++) {
     switch (arr[i]) {
       case '--discard-next':
         i++;
         isCommand = true;
         break;
       case '--discard-prev':
-        if (transformedArray.length > 0 && !isCommand) transformedArray.pop();
+        if (!isCommand && transformedArray.length > 0) transformedArray.pop();
         isCommand = true;
-        break;
-      case '--double-prev':
-        if (i !== 0) {
-          if (isCommand) {
-            isCommand = true;
-            break;
-          }
-          transformedArray.push(arr[i - 1]);
-        }
         break;
       case '--double-next':
-        if (arr.length > i + 1) transformedArray.push(arr[i + 1]);
         isCommand = true;
-
+        if (i + 1 < arr.length) transformedArray.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if (i !== 0 && !isCommand) transformedArray.push(arr[i - 1]);
+        isCommand = true;
         break;
       default:
         isCommand = false;
         transformedArray.push(arr[i]);
     }
-  if (transformedArray.length === 0) return arr;
+  }
+
   return transformedArray;
 };
