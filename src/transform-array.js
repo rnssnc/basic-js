c8dc7994a858ea8f7d971f354f7ad323ec60fd5d
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-01-23 01:48:20 +0300

    feat: solve tranform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('Wrong type');
+  if (arr.length === 0) return [];
+  let transformedArray = [];
+  let isCommand = false;
+
+  for (let i = 0; i < arr.length; i++)
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        isCommand = true;
+        break;
+      case '--discard-prev':
+        if (transformedArray.length > 0 && !isCommand) transformedArray.pop();
+        isCommand = true;
+        break;
+      case '--double-prev':
+        if (i !== 0) {
+          if (isCommand) {
+            isCommand = true;
+            break;
+          }
+          transformedArray.push(arr[i - 1]);
+        }
+        break;
+      case '--double-next':
+        if (arr.length > i + 1) transformedArray.push(arr[i + 1]);
+        isCommand = true;
+
+        break;
+      default:
+        isCommand = false;
+        transformedArray.push(arr[i]);
+    }
+  if (transformedArray.length === 0) return arr;
+  return transformedArray;
 };
