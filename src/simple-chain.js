a458db9dfc193fff59629b958467d1784cf41b44
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-01-23 03:19:53 +0300

    feat: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,51 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 const chainMaker = {
+  chains: [],
+
+  checkValue(value) {
+    typeof value !== 'undefined' ? value : (value = ' ');
+    return value === null ? '( null )' : `( ${value} )`;
+  },
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chains.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chains.push(this.checkValue(value));
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (
+      Number.isInteger(position) &&
+      position <= this.chains.length &&
+      position > 0
+    )
+      this.chains.splice(position - 1, 1);
+    else {
+      this.chains = [];
+      throw new Error();
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chains.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let result = this.chains.join('~~');
+    this.chains = [];
+    return result;
+  },
 };
 
+console.log(
+  [
+    '( 3rd )',
+    '( 2nd )',
+    '( function() {} )',
+    '( DEF )',
+    '( null )',
+    '( [object Object] )',
+  ].join('~~')
+);
 module.exports = chainMaker;
