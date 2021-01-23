7b3ed9ff9022b28c1c5e7a1d15fd2ed6711c60ca
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-01-23 18:34:29 +0300

    feat: solve vigenere-cipher task (needs refactor)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,63 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
+const LETTERS_COUNT = 26;
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+
+  isLetter(code) {
+    if (code >= 65 && code <= 90) return true;
+    return false;
+  }
+
+  validateArguments(message, key) {
+    if (!message && !key) throw new Error();
+  }
+
+  encrypt(message, key) {
+    this.validateArguments(message, key);
+    let result = [];
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let index = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.isLetter(message.charCodeAt(i))) {
+        result.push(
+          String.fromCharCode(
+            65 +
+              ((key.charCodeAt(index) + message.charCodeAt(i)) % LETTERS_COUNT)
+          )
+        );
+        if (index == key.length - 1) index = -1;
+        index++;
+      } else result.push(message[i]);
+    }
+    return this.isDirect ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    this.validateArguments(message, key);
+
+    let result = [];
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let index = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.isLetter(message.charCodeAt(i))) {
+        result.push(
+          String.fromCharCode(
+            65 +
+              ((LETTERS_COUNT - key.charCodeAt(index) + message.charCodeAt(i)) %
+                LETTERS_COUNT)
+          )
+        );
+        if (index == key.length - 1) index = -1;
+        index++;
+      } else result.push(message[i]);
+    }
+
+    return this.isDirect ? result.join('') : result.reverse().join('');
   }
 }
 
